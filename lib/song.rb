79224f0f22db7ec32d45259f42d3687827d18357
Daniel Macharia <danielhuios@gmail.com>
2022-10-26 16:07:41 +0300

    regactpred to use tally

--- lib/song.rb
+++ lib/song.rb
@@ -28,28 +28,34 @@ class Song
         @@genres.uniq
     end
 
+    # def self.genre_count
+    #     genre_count = {}
+    #     @@genres.each do |genre|
+    #         if genre_count[genre]
+    #             genre_count[genre] += 1
+    #         else 
+    #             genre_count[genre] = 1
+    #         end
+    #     end
+    #     genre_count
+    # end
     def self.genre_count
-        genre_count = {}
-        @@genres.each do |genre|
-            if genre_count[genre]
-                genre_count[genre] += 1
-            else 
-                genre_count[genre] = 1
-            end
-        end
-        genre_count
+        @@genres.tally       
     end
 
+    # def self.artist_count
+    #     artist_count = {}
+    #     @@artists.each do |artist|
+    #         if artist_count[artist]
+    #             artist_count[artist] += 1
+    #         else
+    #             artist_count[artist] = 1
+    #         end
+    #     end
+    #     artist_count
+    # end
     def self.artist_count
-        artist_count = {}
-        @@artists.each do |artist|
-            if artist_count[artist]
-                artist_count[artist] += 1
-            else
-                artist_count[artist] = 1
-            end
-        end
-        artist_count
+       @@artists.tally
     end
 end
 
@@ -59,4 +65,4 @@ dance = Song.new("DanceDance", "Sinach", "gospel")
 congratulations = Song.new("Congratulations", "Ada", "gospel")
 rejoice = Song.new("Rejoice", "Sinach", "gospel")
 
-# binding.pry
+binding.pry
